7c8e721d45e1748e22683f3f302ba225114b3a99
Basementbrb <tim@vane-tempest.net>
2015-03-01 13:13:42 +0000

    asd

--- IP2Group11/Assets/scripts/towerBehaviour.cs
+++ IP2Group11/Assets/scripts/towerBehaviour.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class towerBehaviour : MonoBehaviour {
 
 	public GameObject projectile;
+	public Collider2D target;
 
 	// Use this for initialization
 	void Start () {
@@ -12,25 +13,30 @@ public class towerBehaviour : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+	}
+	IEnumerator TowerFire()
+	{
+		Debug.Log ("Tower fired");
+		GameObject g = (GameObject)Instantiate(projectile, transform.position, Quaternion.identity);
+		g.GetComponent<projectileBehaviour>().target = target.transform;
+		yield return new WaitForSeconds(2);
 	}
 
 	void OnTriggerStay2D(Collider2D other)
 	{
 		if(other.gameObject.tag=="Creep")
 		{
-			Debug.Log ("hello");
-			GameObject g = (GameObject)Instantiate(projectile, transform.position, Quaternion.identity);
-			g.GetComponent<projectileBehaviour>().target = other.transform;
+			target = other;
+			StartCoroutine(TowerFire ());
 		}
 	}
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if(other.gameObject.tag=="Creep")
 		{
-			Debug.Log ("hello");
-			GameObject g = (GameObject)Instantiate(projectile, transform.position, Quaternion.identity);
-			g.GetComponent<projectileBehaviour>().target = other.transform;
+			target = other;
+			StartCoroutine(TowerFire ());
 		}
 	}
 }
