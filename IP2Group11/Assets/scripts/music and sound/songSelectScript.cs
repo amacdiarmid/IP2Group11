a7160ef330f7c2fe158f2983fe27211d913635b9
Basementbrb <tim@vane-tempest.net>
2015-03-29 10:53:39 +0100

    scene persistent background music and song selection in options

---
+++ IP2Group11/Assets/scripts/music and sound/songSelectScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class songSelectScript : MonoBehaviour {
+
+	private backgroundMusic music;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ChangeSong (int number) 
+	{
+		music = GameObject.FindWithTag ("Music").GetComponent<backgroundMusic>();
+		music.SetChoice(number);
+	}
+}
